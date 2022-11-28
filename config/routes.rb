bd496b8696bd940993df277fc960423d73f0b007
myles <pagemyles31@gmail.com>
2022-11-28 11:13:13 -0700

    more trails

--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,16 @@ Rails.application.routes.draw do
   get "/ice", to: "pages#ice"
   get "/manitou", to: "pages#manitou"
   
+  get "/emerald", to: "pages#emerald"
+  get "/stmary", to: "pages#stmary"
+  get "/bierstadt", to: "pages#bierstadt"
+  get "/quandary", to: "pages#quandary"
+  get "/high", to: "pages#high"
+  
+  get "/maroon", to: "pages#maroon"
+  get "/independence", to: "pages#independence"
+  get "/flatiron", to: "pages#flatiron"
+  get "/hanging", to: "pages#hanging"
+  get "/pulpit", to: "pages#pulpit"
+  
 end
